7a628704ff3f665c943e995562546d1823e043b5
Sashank <sashanktatvolu@gmail.com>
2022-12-06 10:07:46 +0530

    firstcommit

---
+++ product.py
@@ -0,0 +1,10 @@
+product1= float(input("enter product1 quantity "))
+product2= float(input("enter product2 quantity "))    
+product3= float(input("enter product3 quantity "))
+priceOfProduct1 = float(input("enter product1 price "))
+priceOfProduct2 = float(input("enter product1 price "))
+priceOfProduct3 = float(input("enter product1 price "))
+totalquantity=product1+product2+product3
+total =(priceOfProduct1*product1+ priceOfProduct2*product2+ priceOfProduct3*product3 )
+print("the total quantity is",totalquantity)
+print("the total amount is",total)
