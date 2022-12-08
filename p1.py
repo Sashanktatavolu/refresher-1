1dcfd733db7640ad9cc9675d3fec15bd80e43296
Sashank <sashanktatvolu@gmail.com>
2022-12-08 11:20:18 +0530

    price declared

---
+++ p1.py
@@ -0,0 +1,58 @@
+#if else
+# n=10
+#if n<=0:
+# if (n<=0):
+#     print("less than zero")
+# elif (n==10):
+#     print("value less than equal to five") 
+# else:
+#     print('n is higher than ten')  
+
+# pronouns =['me','her','she','him','he','we','our']
+# print('the pronouns are:')
+# for s in pronouns:
+#     print(s)     
+
+l=[1,5,6.7,8,9,'me',True]
+print(l)
+# print(l[0])
+# print(l[5])
+# print(l[1:3]) #include 1 and exclude 3
+# print(l[1:5])
+# print(l[2:])
+# print(l*2)
+l1=['mine','we']
+# print(l1+l)
+
+t=(1,5,6.7,8,9,'me',True)
+# print(t[0])
+# print(t[1:4])
+# print(t[2:])
+details= {'ran' :90,'me':80,'we':70,'us':20 }
+# print(details)
+# print(details.values())
+# print(details.keys())
+# key = student name
+# value = marks
+# anik = 60
+# details[4]="test"
+# print(details[4])
+# print(details)
+
+# dict ={
+#     "college":"aditya",
+#     "location":"Andhra Pradesh",
+#     "stream":"ECE",
+# }
+# for info in  dict:
+#     print(info)
+# for info1 in dict:
+#     print(dict[info1])    
+
+# college={}
+name = input("enter college name:")
+rollno = input("enter Roll number:")
+# college[name]=rollno
+# print(college)
+college1 = {name:rollno}
+print(college1)
--- product.py
+++ product.py
@@ -18,9 +18,9 @@ product3= int(input("enter product3 quantity "))
 # #using the for loop
 # for s in productlist:
 #     print(s) 
-price1=300
-price2=400
-price3=500
+# price1=300
+# price2=400
+# price3=500
     # totalquantity=product1+product2+product3
     # total =(priceOfProduct1*product1+ priceOfProduct2*product2+ priceOfProduct3*product3 )
     # print("the total quantity is",totalquantity)
@@ -31,10 +31,8 @@ if((product1<=0) or (product2<=0) or(product3<=0)):
     print('please enter a positive number')
 else:
     print('product quantity with price')
-    total =(price1*product1+ price2*product2+ price3*product3 )  
-    entries = {product1:200,product2:300,product3:400
-    
-    }
+    total =(200*product1+ 300*product2+ 500*product3 )  
+    entries = {product1:200,product2:300,product3:500}
     for i,p in entries.items():
         print(i,p)
     print('the amount that you nedd to pay :')
